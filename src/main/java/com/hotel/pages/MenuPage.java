a1276812d2a0f50c34bfb65a654b5a1e1c264c4d
Sasha <backmanil@gmail.com>
2018-10-01 21:24:27 +0300

    Добавили номер комнаты в параметры страницы

--- src/main/java/com/hotel/pages/MenuPage.java
+++ src/main/java/com/hotel/pages/MenuPage.java
@@ -2,16 +2,18 @@ package com.hotel.pages;
 
 import com.hotel.models.Room;
 import com.hotel.models.Stage;
+import com.hotel.repositories.RoomRepository;
 import com.hotel.repositories.StageRepository;
 import org.primefaces.model.menu.DefaultMenuItem;
 import org.primefaces.model.menu.DefaultMenuModel;
 import org.primefaces.model.menu.DefaultSubMenu;
-import org.primefaces.model.menu.MenuModel;
+import org.springframework.data.domain.Example;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.List;
+import java.util.Optional;
 
 @Named
 public class MenuPage {
@@ -19,7 +21,14 @@ public class MenuPage {
     @Inject
     private StageRepository stageRepository;
 
+    @Inject
+    private RoomRepository roomRepository;
+
     private DefaultMenuModel menu;
+
+    private String selectedRoomNumber;
+
+    private Room selectedRoom;
 
     @PostConstruct
     public void init() {
@@ -31,10 +40,42 @@ public class MenuPage {
             menu.addElement(stageMenu);
 
             for (Room room : stage.getRooms()) {
-                DefaultMenuItem itemRoom = new DefaultMenuItem(room.getId());
+                DefaultMenuItem itemRoom = new DefaultMenuItem(room.getNumber());
+                itemRoom.setUrl("menu.xhtml?room=" + room.getNumber());
                 stageMenu.addElement(itemRoom);
             }
         }
+    }
+
+    public void selectRoomFromDatabase() {
+        if (selectedRoomNumber == null || selectedRoomNumber.equals("")) {
+            return;
+        }
+
+        Room example = new Room();
+        example.setNumber(selectedRoomNumber);
+
+        Optional<Room> roomOptinal = roomRepository.findOne(Example.of(example));
+        if (roomOptinal.isPresent()) {
+            selectedRoom = roomOptinal.get();
+            System.out.println("AAAAAAAA " + selectedRoom.getNumber());
+        }
+    }
+
+    public String getSelectedRoomNumber() {
+        return selectedRoomNumber;
+    }
+
+    public void setSelectedRoomNumber(String selectedRoomNumber) {
+        this.selectedRoomNumber = selectedRoomNumber;
+    }
+
+    public Room getSelectedRoom() {
+        return selectedRoom;
+    }
+
+    public void setSelectedRoom(Room selectedRoom) {
+        this.selectedRoom = selectedRoom;
     }
 
     public StageRepository getStageRepository() {
