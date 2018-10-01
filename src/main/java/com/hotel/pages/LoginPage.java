1d4e482470445fb9b794e61877d6dfd73d5d85b0
Sasha <backmanil@gmail.com>
2018-10-01 20:48:11 +0300

    заполнили базу данных этажами

--- src/main/java/com/hotel/pages/LoginPage.java
+++ src/main/java/com/hotel/pages/LoginPage.java
@@ -32,23 +32,72 @@ public class LoginPage {
         stage.setRooms(new ArrayList<>());
         Room room1 = new Room();
         Room room2 = new Room();
+        Room room3 = new Room();
+        Room room4 = new Room();
+
         stage.getRooms().add(room1);
         stage.getRooms().add(room2);
+        stage.getRooms().add(room3);
+        stage.getRooms().add(room4);
+
         room1.setNumber("100");
-        room1.setRoomType(RoomType.STANDART);
+        room1.setRoomType(RoomType.ECONOM);
         room1.setFree("2");
         room1.setStage(stage);
+
         room2.setNumber("101");
-        room2.setRoomType(RoomType.STANDART);
+        room2.setRoomType(RoomType.ECONOM);
         room2.setFree("2");
         room2.setStage(stage);
+        room3.setNumber("102");
+        room3.setRoomType(RoomType.STANDART);
+        room3.setFree("2");
+        room3.setStage(stage);
+        room4.setNumber("103");
+        room4.setRoomType(RoomType.STANDART);
+        room4.setFree("2");
+        room4.setStage(stage);
+
         stageRepository.save(stage);
-        System.out.println("save");
 
         Registration registration = new Registration();
         registration.setName("111");
         registration.setPassword("111");
         registrationRepository.save(registration);
+
+
+        stage = new Stage();
+        stage.setNumber("2A");
+        stage.setRooms(new ArrayList<>());
+        room1 = new Room();
+        room2 = new Room();
+        room3 = new Room();
+        room4 = new Room();
+
+        stage.getRooms().add(room1);
+        stage.getRooms().add(room2);
+        stage.getRooms().add(room3);
+        stage.getRooms().add(room4);
+
+        room1.setNumber("200");
+        room1.setRoomType(RoomType.STANDART);
+        room1.setFree("2");
+        room1.setStage(stage);
+
+        room2.setNumber("201");
+        room2.setRoomType(RoomType.STANDART);
+        room2.setFree("2");
+        room2.setStage(stage);
+        room3.setNumber("202");
+        room3.setRoomType(RoomType.STANDART);
+        room3.setFree("3");
+        room3.setStage(stage);
+        room4.setNumber("203");
+        room4.setRoomType(RoomType.STANDART);
+        room4.setFree("5");
+        room4.setStage(stage);
+
+        stageRepository.save(stage);
     }
 
     private String login;
