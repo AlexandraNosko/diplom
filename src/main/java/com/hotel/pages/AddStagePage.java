ee5cca87d373a087313bd5a7fcd664226db0eff7
Sasha <backmanil@gmail.com>
2018-10-03 21:30:40 +0300

    страница для добавления этажей

---
+++ src/main/java/com/hotel/pages/AddStagePage.java
@@ -0,0 +1,54 @@
+package com.hotel.pages;
+
+import com.hotel.models.Stage;
+import com.hotel.repositories.StageRepository;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+public class AddStagePage {
+
+    @Inject
+    private StageRepository stageRepository;
+
+    private String stageNumber;
+
+    public String getStageNumber() {
+        return stageNumber;
+    }
+
+    public void setStageNumber(String stageNumber) {
+        this.stageNumber = stageNumber;
+    }
+
+    public StageRepository getStageRepository() {
+        return stageRepository;
+    }
+
+    public void setStageRepository(StageRepository stageRepository) {
+        this.stageRepository = stageRepository;
+    }
+
+
+    public String addStage() {
+        if (stageNumber != null && stageNumber.equals("")) {
+            sendMessage("Введите номер этажа");
+            return null;
+        }
+        Stage stage = new Stage();
+        stage.setNumber(stageNumber);
+        stageRepository.save(stage);
+        sendMessage("Этаж успешно сохранен");
+        return "goToMenu";
+    }
+
+
+    public void sendMessage(String message) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, message, null));
+
+    }
+}
