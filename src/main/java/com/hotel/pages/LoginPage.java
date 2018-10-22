31489dbdc945329fd1b18434b0e98023ef9d6781
Sasha <backmanil@gmail.com>
2018-10-22 20:11:21 +0300

    изменения на логин странице

--- src/main/java/com/hotel/pages/LoginPage.java
+++ src/main/java/com/hotel/pages/LoginPage.java
@@ -1,210 +1,30 @@
 package com.hotel.pages;
 
-import com.hotel.models.Registration;
-import com.hotel.models.Room;
-import com.hotel.models.RoomType;
-import com.hotel.models.Stage;
-import com.hotel.repositories.RegistrationRepository;
-import com.hotel.repositories.StageRepository;
-import org.apache.commons.lang3.time.DateUtils;
-import org.springframework.data.domain.Example;
-
-import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-import javax.inject.Inject;
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Optional;
+import java.io.Serializable;
 
 @Named
-public class LoginPage {
+@ViewScoped
+public class LoginPage implements Serializable {
 
-    @Inject
-    private RegistrationRepository registrationRepository;
+    private String error;
 
-    @Inject
-    private StageRepository stageRepository;
-
-    @PostConstruct
-    public void init() {
-        Stage stage = new Stage();
-        stage.setNumber("1A");
-        stage.setRooms(new ArrayList<>());
-        Room room1 = new Room();
-        Room room2 = new Room();
-        Room room3 = new Room();
-        Room room4 = new Room();
-
-        stage.getRooms().add(room1);
-        stage.getRooms().add(room2);
-        stage.getRooms().add(room3);
-        stage.getRooms().add(room4);
-
-        room1.setNumber("100");
-        room1.setArrivalDate(DateUtils.addDays(new Date(), -20));
-        room1.setDepartureDate(DateUtils.addDays(new Date(), 20));
-        room1.setGuestsCount(2);
-        room1.setRoomType(RoomType.ECONOM);
-        room1.setTotalNumberOfSeats(6);
-        room1.setCost(400);
-        room1.setPaid(400);
-        room1.setStage(stage);
-
-        room2.setNumber("101");
-        room2.setArrivalDate(DateUtils.addDays(new Date(), -4));
-        room2.setDepartureDate(DateUtils.addDays(new Date(), 5));
-        room2.setGuestsCount(3);
-        room2.setRoomType(RoomType.ECONOM);
-        room2.setTotalNumberOfSeats(6);
-        room2.setCost(500);
-        room2.setPaid(400);
-        room2.setStage(stage);
-
-        room3.setNumber("102");
-        room3.setGuestsCount(0);
-        room3.setRoomType(RoomType.ECONOM);
-        room3.setTotalNumberOfSeats(6);
-        room3.setCost(500);
-        room3.setPaid(0);
-        room3.setStage(stage);
-
-        room4.setNumber("103");
-        room4.setGuestsCount(0);
-        room4.setRoomType(RoomType.STANDART);
-        room4.setTotalNumberOfSeats(2);
-        room4.setCost(550);
-        room4.setPaid(0);
-        room4.setStage(stage);
-
-        stageRepository.save(stage);
-
-        Registration registration = new Registration();
-        registration.setName("111");
-        registration.setPassword("111");
-        registrationRepository.save(registration);
-
-
-        stage = new Stage();
-        stage.setNumber("2A");
-        stage.setRooms(new ArrayList<>());
-        room1 = new Room();
-        room2 = new Room();
-        room3 = new Room();
-        room4 = new Room();
-
-        stage.getRooms().add(room1);
-        stage.getRooms().add(room2);
-        stage.getRooms().add(room3);
-        stage.getRooms().add(room4);
-
-        room1.setNumber("200");
-        room1.setArrivalDate(DateUtils.addDays(new Date(), -7));
-        room1.setDepartureDate(DateUtils.addDays(new Date(), 5));
-        room1.setGuestsCount(2);
-        room1.setRoomType(RoomType.STANDART);
-        room1.setTotalNumberOfSeats(3);
-        room1.setCost(550);
-        room1.setPaid(400);
-        room1.setStage(stage);
-
-        room2.setNumber("201");
-        room2.setArrivalDate(DateUtils.addDays(new Date(), -9));
-        room2.setDepartureDate(DateUtils.addDays(new Date(), 1));
-        room2.setGuestsCount(2);
-        room2.setRoomType(RoomType.STANDART);
-        room2.setTotalNumberOfSeats(3);
-        room2.setCost(600);
-        room2.setPaid(400);
-        room2.setStage(stage);
-
-        room3.setNumber("202");
-        room3.setGuestsCount(0);
-        room3.setRoomType(RoomType.STANDART);
-        room3.setTotalNumberOfSeats(4);
-        room3.setCost(700);
-        room3.setPaid(0);
-        room3.setStage(stage);
-
-        room4.setNumber("203");
-        room4.setGuestsCount(0);
-        room4.setRoomType(RoomType.STANDART);
-        room4.setTotalNumberOfSeats(4);
-        room4.setCost(900);
-        room4.setPaid(0);
-        room4.setStage(stage);
-
-        stageRepository.save(stage);
-
-        stage = new Stage();
-        stage.setNumber("3A");
-        stage.setRooms(new ArrayList<>());
-        room1 = new Room();
-        room2 = new Room();
-
-        stage.getRooms().add(room1);
-        stage.getRooms().add(room2);
-
-        room1.setNumber("300");
-        room1.setGuestsCount(0);
-        room1.setRoomType(RoomType.VIP);
-        room1.setTotalNumberOfSeats(2);
-        room1.setCost(2000);
-        room1.setPaid(0);
-        room1.setStage(stage);
-
-        room2.setNumber("301");
-        room2.setArrivalDate(DateUtils.addDays(new Date(), -5));
-        room2.setDepartureDate(new Date());
-        room2.setGuestsCount(3);
-        room2.setRoomType(RoomType.VIP);
-        room2.setTotalNumberOfSeats(5);
-        room2.setCost(3000);
-        room2.setPaid(2500);
-        room2.setStage(stage);
-
-        stageRepository.save(stage);
+    public String getError() {
+        return error;
     }
 
-    private String login;
-
-    private String password;
-
-    public String getLogin() {
-        return login;
+    public void setError(String error) {
+        this.error = error;
     }
 
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String doLogin() {
-        Registration example = new Registration();
-        example.setName(login);
-        example.setPassword(password);
-        Optional<Registration> exists = registrationRepository.findOne(Example.of(example));
-        if (!exists.isPresent()) {
-            sendMessage("Логин или пароль не правильны");
-            return null;
+    public void showError() {
+        if (error != null && !error.trim().isEmpty()) {
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN,
+                    "Неправильный логин или пароль", null));
         }
-
-        return "goToMenu";
     }
-
-    public void sendMessage(String message) {
-        FacesContext context = FacesContext.getCurrentInstance();
-
-        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, message, null));
-    }
-
-
 }
